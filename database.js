94c0f2672f697bbba28f3c96cca59ad4a41c1de0
Jacqueline Williamson <jacqueline_25@hotmail.com>
2020-08-26 08:50:06 -0400

    trying to get pic to load through js...should have started with something easier

---
+++ database.js
@@ -0,0 +1,16 @@
+
+const ulElement = document.querySelector("#customer-database")
+
+for (let customer of customers){
+    const listElement = document.createElement("li")
+    const imageEL = document.createElement("img")
+    listElement.appendChild(imageEl)
+    console.log(listElement)}
+
+    function createImageElement (url)
+    imageEL.src = "https://randomuser.me/api/portraits/thumb/men/0.jpg"
+    return imageEl
+
+
+//I'm attempting here to at least get one imagine to appear as a list element, but I'm obviously missing something.
+//I've looked at this to a point where I'm doubting if any of it is correct/valid!
